81184c998a89addabea0ec25e768b87e9c9e1d28
kerimcetinbas <a.kerim.cetinbas@yandex.com>
2023-02-15 15:22:09 +0300

    try with trycatch

--- index.js
+++ index.js
@@ -5,8 +5,12 @@ const { connect, PasswordAuthenticator } = require("couchbase")
 
     ; (async () => {
         
-        const cluster = await connect("couchbase://localhost", new PasswordAuthenticator("administrator", "administrator"))
+       try {
+         const cluster = await connect("couchbase://localhost", new PasswordAuthenticator("administrator", "administrator"))
+       } catch (error) {
+            console.log("error")
+       }
  
 
-        console.log("cluster" ,cluster)
+      
     })()  
