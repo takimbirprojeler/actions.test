62f13b0f4467bdcc3b7f1dda122b54bb57ae049d
kerimcetinbas <a.kerim.cetinbas@yandex.com>
2023-02-15 15:07:49 +0300

    24

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { connect, PasswordAuthenticator } = require("couchbase")
 
     ; (async () => {
         
-        const cluster = await connect("couchbase://127.0.0.1:8091", new PasswordAuthenticator("administrator", "administrator"))
+        const cluster = await connect("couchbase://127.0.0.1", new PasswordAuthenticator("administrator", "administrator"))
  
 
         console.log("cluster" ,cluster)
